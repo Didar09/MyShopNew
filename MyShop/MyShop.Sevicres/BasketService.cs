8948a9dc818148f81b41493833fcaa80ff88dd58
Didar Singh <didar.singh@systematixindia.com>
2020-11-21 15:33:52 +0530

    Added order Creation

--- MyShop/MyShop.Sevicres/BasketService.cs
+++ MyShop/MyShop.Sevicres/BasketService.cs
@@ -146,5 +146,11 @@ namespace MyShop.Sevicres
                 return model;
             }
         }
+        public void ClearBasket(HttpContextBase httpContext)
+        {
+            Basket basket = GetBasket(httpContext,false);
+            basket.BasketItems.Clear();
+            basketContext.Commit();
+        }
     }
 }
