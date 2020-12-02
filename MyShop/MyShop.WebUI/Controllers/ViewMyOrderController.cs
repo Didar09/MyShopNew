af02085fce8874443fa3186e8b0a45281d396352
Didar Singh <didar.singh@systematixindia.com>
2020-12-02 10:59:08 +0530

    Order added

---
+++ MyShop/MyShop.WebUI/Controllers/ViewMyOrderController.cs
@@ -0,0 +1,42 @@
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyShop.WebUI.Controllers
+{
+    [Authorize]
+    public class ViewMyOrderController : Controller
+    {
+        IOrderService orderService;
+        IRepository<Customer> customers;
+        public ViewMyOrderController(IOrderService OrderService, IRepository<Customer> Customers)
+        {
+            this.orderService = OrderService;
+            this.customers = Customers;
+        }
+        // GET: ViewMyOrder
+        public ActionResult Index()
+        {
+            List<Order> orders = orderService.GetOrderList().Where(u=>u.Email==User.Identity.Name).ToList();
+            return View(orders);
+        }
+        public ActionResult Detail(string Id)
+        {
+            Order order = orderService.GetOrder(Id);
+            return View(order);
+        }
+        public ActionResult NewList()
+        {
+            return View();
+        }
+        public ActionResult Datatable()
+        {
+            var orderList = orderService.GetOrderList();
+            return Json(new { data = orderList }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
